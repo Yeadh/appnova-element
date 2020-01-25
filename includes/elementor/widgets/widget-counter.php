f972d10350b113b8299b8038401026b49bc9a2bd
themebing <themebing@gmail.com>
2020-01-25 12:27:06 +0600

    Update widget-counter.php

--- includes/elementor/widgets/widget-counter.php
+++ includes/elementor/widgets/widget-counter.php
@@ -80,7 +80,7 @@ class appnova_Widget_Counter extends Widget_Base {
        
       $settings = $this->get_settings_for_display(); ?>
 
-      <section class="counter-area inner-counter-bg counter-bg pt-150 pb-100" data-background="<?php echo get_template_directory_uri() ?>/images/counter_bg.jpg">
+      <section class="counter-area inner-counter-bg counter-bg pt-100 pb-50" data-background="<?php echo get_template_directory_uri() ?>/images/counter_bg.jpg">
          <div class="container">
            <div class="row">
             <?php foreach (  $settings['counter'] as $counter_single ): ?>
